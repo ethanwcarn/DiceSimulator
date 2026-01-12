8fd16d4a3e477c9735ac61bbc477fedd325eb200
ethanwcarn <carn0@byu.edu>
2026-01-12 10:00:30 -0700

    Add files via upload
    new versions with better comments

--- DiceSimulator.cs
+++ DiceSimulator.cs
@@ -4,11 +4,13 @@ namespace DiceRollingSimulator
 {
     class DiceSimulator
     {
+        // number of rolls taken from program.cs 
         public int[] RollDice(int numberOfRolls)
         {
             int[] rollResults = new int[11]; // indices 0–10 represent totals 2–12
             Random random = new Random();
 
+            // After getting a random number for each die, add them up
             for (int i = 0; i < numberOfRolls; i++)
             {
                 int die1 = random.Next(1, 7);
@@ -17,7 +19,7 @@ namespace DiceRollingSimulator
 
                 rollResults[total - 2]++;
             }
-
+            //return the results
             return rollResults;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿// Ethan Carn IS 413 Dice rolling simulator 
+// using system and namespace just set up default needs
+using System;
 
 namespace DiceRollingSimulator
 {
     class Program
     {
+        // Pass an empty string
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the dice throwing simulator!");
             Console.Write("How many dice rolls would you like to simulate? ");
 
+            // set number of rolls as a number based on user input 
             int numberOfRolls = int.Parse(Console.ReadLine());
 
             // Create simulator and run rolls
@@ -22,11 +26,13 @@ namespace DiceRollingSimulator
             Console.WriteLine();
 
             // Results array index 0 corresponds to roll total of 2
+            // The for loop will continue for as many rolls the user said to complete
             for (int i = 0; i < results.Length; i++)
             {
                 int rollTotal = i + 2;
                 int percentage = (int)Math.Round((double)results[i] / numberOfRolls * 100);
 
+                //formatting for how the rolls will appear 
                 Console.Write($"{rollTotal}: ");
                 Console.WriteLine(new string('*', percentage));
             }
